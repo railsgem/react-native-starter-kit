b154c56be2cdaba0327cfc1bcb85e3303b635cb8
YING CHEN <ying.chen@naamtech.com>
2018-02-21 11:42:58 +1100

    rewrite project list as an component

--- src/native/components/Projects.js
+++ src/native/components/Projects.js
@@ -8,91 +8,137 @@ import Error from './Error';
 import Header from './Header';
 import Spacer from './Spacer';
 
-const ProjectListing = ({
-  error,
-  loading,
-  projects,
-  reFetch,
-}) => {
-  // Loading
-  if (loading) return <Loading />;
+class ProjectListing extends React.Component {
+  static propTypes = {
+    error: PropTypes.string,
+    // success: PropTypes.string,
+    loading: PropTypes.bool.isRequired,
+    // onFormSubmit: PropTypes.func.isRequired,
+    projects: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+    reFetch: PropTypes.func,
+    // member: PropTypes.shape({
+    //   firstName: PropTypes.string,
+    //   lastName: PropTypes.string,
+    //   email: PropTypes.string,
+    // }).isRequired,
+  }
 
-  // Error
-  if (error) return <Error content={error} />;
+  static defaultProps = {
+    error: null,
+    // success: null,
+    reFetch: null,
+  }
 
-  const keyExtractor = item => item.id;
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   firstName: props.member.firstName || '',
+    //   lastName: props.member.lastName || '',
+    //   email: props.member.email || '',
+    //   password: '',
+    //   password2: '',
+    //   changeEmail: false,
+    //   changePassword: false,
+    // };
 
-  const onPress = item => Actions.project({ match: { params: { id: String(item.id) } } });
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return (
-    <Container>
-      <Content padder>
-        <Header
-          title="Top Projects"
-          content="This is here to show how you can read and display data from a data source (in our case, Firebase)."
-        />
+  // handleChange = (name, val) => {
+  //   this.setState({
+  //     ...this.state,
+  //     [name]: val,
+  //   });
+  // }
+  //
+  // handleSubmit = () => {
+  //   this.props.onFormSubmit(this.state)
+  //     .then(() => console.log('Profile Updated'))
+  //     .catch(e => console.log(`Error: ${e}`));
+  // }
 
-        <FlatList
-          numColumns={1}
-          data={projects}
-          renderItem={({ item }) => (
-            <Card transparent style={{ paddingHorizontal: 6 }}>
-              <CardItem cardBody>
-                <TouchableOpacity onPress={() => onPress(item)} style={{ flex: 1 }}>
-                  <Image
-                    source={{ uri: item.image }}
-                    style={{
-                      height: 100,
-                      width: null,
-                      flex: 1,
-                      borderRadius: 5,
-                    }}
-                  />
-                </TouchableOpacity>
-              </CardItem>
-              <CardItem cardBody>
-                <Body>
-                  <Spacer size={10} />
-                  <Text style={{ fontWeight: '800' }}>{item.projectName}</Text>
-                  <Spacer size={15} />
-                  <Button
-                    block
-                    bordered
-                    small
-                    onPress={() => onPress(item)}
-                  >
-                    <Text>View Recipe</Text>
-                  </Button>
-                  <Spacer size={5} />
-                </Body>
-              </CardItem>
-            </Card>
-          )}
-          keyExtractor={keyExtractor}
-          refreshControl={
-            <RefreshControl
-              refreshing={loading}
-              onRefresh={reFetch}
-            />
-          }
-        />
+  render() {
+    const {
+      loading,
+      error,
+      reFetch,
+      projects,
+    } = this.props;
 
-        <Spacer size={20} />
-      </Content>
-    </Container>
-  );
-};
+    // Loading
+    if (loading) return <Loading />;
 
-ProjectListing.propTypes = {
-  error: PropTypes.string,
-  loading: PropTypes.bool.isRequired,
-  projects: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-  reFetch: PropTypes.func,
-};
+    // Error
+    if (error) return <Error content={error} />;
 
-ProjectListing.defaultProps = {
-  error: null,
-  reFetch: null,
-};
+    const keyExtractor = item => item.id;
+
+    const onPress = item => Actions.project({ match: { params: { id: String(item.id) } } });
+
+    // const { loading, error, success } = this.props;
+
+    // Loading
+    if (loading) return <Loading />;
+
+    return (
+      <Container>
+        <Content padder>
+          <Header
+            title="Top Projects"
+            content="This is here to show how you can read and display data from a data source (in our case, Firebase)."
+          />
+
+          <FlatList
+            numColumns={1}
+            data={projects}
+            renderItem={({ item }) => (
+              <Card transparent style={{ paddingHorizontal: 6 }}>
+                <CardItem cardBody>
+                  <TouchableOpacity onPress={() => onPress(item)} style={{ flex: 1 }}>
+                    <Image
+                      source={{ uri: item.image }}
+                      style={{
+                        height: 100,
+                        width: null,
+                        flex: 1,
+                        borderRadius: 5,
+                      }}
+                    />
+                  </TouchableOpacity>
+                </CardItem>
+                <CardItem cardBody>
+                  <Body>
+                    <Spacer size={10} />
+                    <Text style={{ fontWeight: '800' }}>{item.projectName}</Text>
+                    <Spacer size={15} />
+                    <Button
+                      block
+                      bordered
+                      small
+                      onPress={() => onPress(item)}
+                    >
+                      <Text>View Recipe</Text>
+                    </Button>
+                    <Spacer size={5} />
+                  </Body>
+                </CardItem>
+              </Card>
+            )}
+            keyExtractor={keyExtractor}
+            refreshControl={
+              <RefreshControl
+                refreshing={loading}
+                onRefresh={reFetch}
+              />
+            }
+          />
+
+          <Spacer size={20} />
+        </Content>
+      </Container>
+    );
+  }
+}
 
 export default ProjectListing;
